c235567892df211dd7e505b43140397c696568a5
Philippe De Oliveira <philippe.deoliveira75@gmail.com>
2025-02-07 11:05:31 +0100

    maj api v2

--- src/_services/api.jsx
+++ src/_services/api.jsx
@@ -1,16 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const fetchMovies = async () => {
-
   try {
-    const response = await axios.get('/api/movies')
-    return response.data.results
-  } 
-  
-  catch (error) {
-    console.error('Erreur API Proxy:', error)
-    return []
-
+    const { data } = await axios.get('/api/movies'); // Appelle le Proxy
+    return data.results;
+  } catch (error) {
+    console.error('Erreur API Proxy:', error);
+    return [];
   }
-
-}
+};
